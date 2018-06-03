1d06911c830e6b7da72f27059057b4aa0b733fef
mohamed khamis mohamed salah <mohamed.khamis.salah@gmail.com>
2018-06-03 17:05:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    response_sent_text = message['message']['text']
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
@@ -58,5 +58,4 @@ def send_message(recipient_id, response):
     return "success"
 
 if __name__ == "__main__":
-    predict()
     app.run()
